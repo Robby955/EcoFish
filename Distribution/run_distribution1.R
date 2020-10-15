429c1cbc45df5727156371778b2f3f676538e951
Robert Sneiderman <48200016+Robby955@users.noreply.github.com>
2020-10-15 14:41:22 -0700

    Update run_distribution1.R

--- Distribution/run_distribution1.R
+++ Distribution/run_distribution1.R
@@ -2,6 +2,7 @@
 # This code provides several anomaly checks and outlier detection for eDNA primers. Of particular intrest are eONKI4 and eFISH1.
 
 # Set parameters and load in libraries.
+
 knitr::opts_chunk$set(fig.width=12, fig.height=10) 
 
 suppressMessages(library(tidyverse))  #For general usage and piping
@@ -20,6 +21,7 @@ suppressMessages(library(OutlierDetection)) #For outlier detection
 
 
 # Read in Data and Wrangle.
+# Set to locations of the csv file.
 
 Dat=read.csv('Helbing-LOD_LOQ_DataZero48-040319MJAML.csv',header=TRUE,stringsAsFactors = TRUE) #Read in the data set
 
@@ -72,7 +74,7 @@ for(i in levels(Dat$SQf)){ # Loop over all SQ of intrest
   
   sub.quant= Dat.eFISH %>%
     filter(SQf==i)
-
+  
   sub.quant=sub.quant[!(is.na(sub.quant$Cq)),]
   
   coef_var[entry]=round(sd(sub.quant$Cq)/mean(sub.quant$Cq),4) #Calculate the coefficient of variation
@@ -94,9 +96,9 @@ for(i in levels(Dat$SQf)){ # Loop over all SQ of intrest
   
   entry=entry+1
   
- # if(ad$p.value<0.05 | cvm$p.value<0.05){ #If the p-value is below 0.05 for either test, we should reject the null hypothesis
- #   print("stop")
- # }
+  # if(ad$p.value<0.05 | cvm$p.value<0.05){ #If the p-value is below 0.05 for either test, we should reject the null hypothesis
+  #   print("stop")
+  # }
   cat("\n\n")
 }
 
@@ -124,8 +126,8 @@ for(i in levels(Dat$SQf)){
   
   sub.quant=sub.quant[!(is.na(sub.quant$Cq)),] #Remove missing entries
   
- # qqnorm(log(sub.quant$Cq),main=paste("SQ",i))
- # qqline(log(sub.quant$Cq))
+  # qqnorm(log(sub.quant$Cq),main=paste("SQ",i))
+  # qqline(log(sub.quant$Cq))
   
   
   n=length(sub.quant$Cq)
@@ -136,7 +138,7 @@ for(i in levels(Dat$SQf)){
   
   mle.sigma=sqrt(mle.sigmasq)
   
-
+  
   
   mle.mean=exp(mle.mu+(mle.sigmasq/2)) #Estimate mean using MLE parameters
   
@@ -171,135 +173,135 @@ for(i in levels(Dat$SQf)){
   entry=entry+1
   
   
- # if(ad$p.value<0.05 | cvm$p.value<0.05){ #If the p-value is below 0.05 we should reject the null hypothesis.
+  # if(ad$p.value<0.05 | cvm$p.value<0.05){ #If the p-value is below 0.05 we should reject the null hypothesis.
   #  print(paste("Stop: Failure at SQ:",i))
-#  }
+  #  }
   
   cat("\n\n")
   
 }
-  
-  
-  
-  
-  coef_var=rep(0,length(levels(Dat$SQf))) 
-  coef_var.standard=rep(0,length(levels(Dat$Sqf)))
-  p_val.cvm=rep(0,length(levels(Dat$SQf)))
-  p_val.ad=rep(0,length(levels(Dat$SQf)))
-  
-  entry=1
-  #pdf(file='eFISH1WeibullQQ.pdf')
-  par(mfrow=c(3,2))
-  
-  for(i in levels(Dat$SQf)){
-    
-    sub.quant= Dat.eFISH %>%
-      filter(SQf==i) %>%
-      filter(!is.na(Cq))
-    
-    sub.quant=sub.quant[!(is.na(sub.quant$Cq)),]
-    
-    dat.weib=fitdist(sub.quant$Cq,"weibull") #Fit a Weibull distribution using fitdist to estimate the parameters
-    
-#    qqPlot(qweibull(ppoints(length(sub.quant$Cq)), shape = dat.weib$estimate[1], 
-   #                 scale =  dat.weib$estimate[2]), sub.quant$Cq,add.line=TRUE,xlab="Theoretical Quantiles",ylab="Sample Quantiles",main=paste("SQ",i))
-    
-    
-    cvm=cvm.test(sub.quant$Cq, "pweibull", shape=dat.weib$estimate[1], scale=dat.weib$estimate[2])
-    
-    ad=ad.test(sub.quant$Cq, "pweibull", shape=dat.weib$estimate[1], scale=dat.weib$estimate[2])
-    
-    
-    mle.shape=dat.weib$estimate[1] #alpha
-    
-    mle.scale=dat.weib$estimate[2] #lambda
-    
-    weibull.mean=as.numeric(mle.scale*gamma(1+(1/mle.shape)))
-    
-    weibull.var=as.numeric(mle.scale^2*(gamma(1+(2/mle.shape))-gamma(1+(1/mle.shape)))^2)
-    
-    weibull.sd=sqrt(weibull.var)  
-    
-    
 
-    coef_var[entry]=round(sd(sub.quant$Cq)/mean(sub.quant$Cq),4)
-    
-    
-    coef_var.standard[entry]=round(sd(sub.quant$Cq)/mean(sub.quant$Cq),4)
-    
-    p_val.ad[entry]=round(ad$p.value,4)
-    p_val.cvm[entry]=round(cvm$p.value,4)
-    
-    entry=entry+1
-    
-    
+
+
+
+coef_var=rep(0,length(levels(Dat$SQf))) 
+coef_var.standard=rep(0,length(levels(Dat$Sqf)))
+p_val.cvm=rep(0,length(levels(Dat$SQf)))
+p_val.ad=rep(0,length(levels(Dat$SQf)))
+
+entry=1
+#pdf(file='eFISH1WeibullQQ.pdf')
+par(mfrow=c(3,2))
+
+for(i in levels(Dat$SQf)){
+  
+  sub.quant= Dat.eFISH %>%
+    filter(SQf==i) %>%
+    filter(!is.na(Cq))
+  
+  sub.quant=sub.quant[!(is.na(sub.quant$Cq)),]
+  
+  dat.weib=fitdist(sub.quant$Cq,"weibull") #Fit a Weibull distribution using fitdist to estimate the parameters
+  
+  #    qqPlot(qweibull(ppoints(length(sub.quant$Cq)), shape = dat.weib$estimate[1], 
+  #                 scale =  dat.weib$estimate[2]), sub.quant$Cq,add.line=TRUE,xlab="Theoretical Quantiles",ylab="Sample Quantiles",main=paste("SQ",i))
+  
+  
+  cvm=cvm.test(sub.quant$Cq, "pweibull", shape=dat.weib$estimate[1], scale=dat.weib$estimate[2])
+  
+  ad=ad.test(sub.quant$Cq, "pweibull", shape=dat.weib$estimate[1], scale=dat.weib$estimate[2])
+  
+  
+  mle.shape=dat.weib$estimate[1] #alpha
+  
+  mle.scale=dat.weib$estimate[2] #lambda
+  
+  weibull.mean=as.numeric(mle.scale*gamma(1+(1/mle.shape)))
+  
+  weibull.var=as.numeric(mle.scale^2*(gamma(1+(2/mle.shape))-gamma(1+(1/mle.shape)))^2)
+  
+  weibull.sd=sqrt(weibull.var)  
+  
+  
+  
+  coef_var[entry]=round(sd(sub.quant$Cq)/mean(sub.quant$Cq),4)
+  
+  
+  coef_var.standard[entry]=round(sd(sub.quant$Cq)/mean(sub.quant$Cq),4)
+  
+  p_val.ad[entry]=round(ad$p.value,4)
+  p_val.cvm[entry]=round(cvm$p.value,4)
+  
+  entry=entry+1
+  
+  
   #  if(ad$p.value<0.05 | cvm$p.value<0.05){
-     # print("stop")
-#}
-    }
-    
-    
-
-    coef_var_ll=rep(0,length(levels(Dat$SQf)))
-    coef_var=rep(0,length(levels(Dat$SQf)))
-    p_val.cvm=rep(0,length(levels(Dat$SQf)))
-    p_val.ad=rep(0,length(levels(Dat$SQf)))
-    entry=1
-    
-
-    
-    for(i in levels(Dat$SQf)){
-      
-      sub.quant= Dat.eFISH %>%
-        filter(SQf==i) %>%
-        filter(!is.na(Cq))
-      
-      sub.quant=sub.quant[!(is.na(sub.quant$Cq)),]
-      
-      
-      dat.loglogis=fitdist(sub.quant$Cq,"llogis")
-      
-     # qqPlot(qllogis(ppoints(length(sub.quant$Cq)), shape = dat.loglogis$estimate[1], 
-     #                scale =  dat.loglogis$estimate[2]), log(sub.quant$Cq),add.line=TRUE,xlab="Theoretical Quantiles",ylab="Sample Quantiles",main=paste("SQ",i))
-      
-      
-      
-      cvm=cvm.test(sub.quant$Cq, "pllogis", shape = dat.loglogis$estimate[1], scale =  dat.loglogis$estimate[2])
-      
-      ad=ad.test(sub.quant$Cq, "pllogis", shape = dat.loglogis$estimate[1], scale =  dat.loglogis$estimate[2])
-      
-      
-      mle.shape=as.numeric(dat.loglogis$estimate[1]) #Beta
-      
-      mle.scale=as.numeric(dat.loglogis$estimate[2]) #Alpha
-      
-      
-      b=as.numeric(pi/mle.shape)
-      
-      loglogis.mean=(mle.scale*b)/sin(b)
-      
-      loglogis.var=(mle.scale^2)*((2*b/sin(2*b))-(b^2/sin(b)^2))
-      
-      loglogis.sd=sqrt(loglogis.var)
-      
-      
-      coef_var_ll[entry]=round(loglogis.sd/loglogis.mean,4)
-      coef_var[entry]=round(sd(sub.quant$Cq)/mean(sub.quant$Cq),4)
-      
-      p_val.ad[entry]=round(ad$p.value,4)
-      p_val.cvm[entry]=round(cvm$p.value,4)
-      
-      entry=entry+1
-
-      #if(ad$p.value<0.05 | cvm$p.value<0.05){
-      #  print(paste("Log-Logistic does not fit at SQ:",i))
-     # }
-    #  cat("\n\n")
-      
+  # print("stop")
+  #}
 }
 
-    
-    
+
+
+coef_var_ll=rep(0,length(levels(Dat$SQf)))
+coef_var=rep(0,length(levels(Dat$SQf)))
+p_val.cvm=rep(0,length(levels(Dat$SQf)))
+p_val.ad=rep(0,length(levels(Dat$SQf)))
+entry=1
+
+
+
+for(i in levels(Dat$SQf)){
+  
+  sub.quant= Dat.eFISH %>%
+    filter(SQf==i) %>%
+    filter(!is.na(Cq))
+  
+  sub.quant=sub.quant[!(is.na(sub.quant$Cq)),]
+  
+  
+  dat.loglogis=fitdist(sub.quant$Cq,"llogis")
+  
+  # qqPlot(qllogis(ppoints(length(sub.quant$Cq)), shape = dat.loglogis$estimate[1], 
+  #                scale =  dat.loglogis$estimate[2]), log(sub.quant$Cq),add.line=TRUE,xlab="Theoretical Quantiles",ylab="Sample Quantiles",main=paste("SQ",i))
+  
+  
+  
+  cvm=cvm.test(sub.quant$Cq, "pllogis", shape = dat.loglogis$estimate[1], scale =  dat.loglogis$estimate[2])
+  
+  ad=ad.test(sub.quant$Cq, "pllogis", shape = dat.loglogis$estimate[1], scale =  dat.loglogis$estimate[2])
+  
+  
+  mle.shape=as.numeric(dat.loglogis$estimate[1]) #Beta
+  
+  mle.scale=as.numeric(dat.loglogis$estimate[2]) #Alpha
+  
+  
+  b=as.numeric(pi/mle.shape)
+  
+  loglogis.mean=(mle.scale*b)/sin(b)
+  
+  loglogis.var=(mle.scale^2)*((2*b/sin(2*b))-(b^2/sin(b)^2))
+  
+  loglogis.sd=sqrt(loglogis.var)
+  
+  
+  coef_var_ll[entry]=round(loglogis.sd/loglogis.mean,4)
+  coef_var[entry]=round(sd(sub.quant$Cq)/mean(sub.quant$Cq),4)
+  
+  p_val.ad[entry]=round(ad$p.value,4)
+  p_val.cvm[entry]=round(cvm$p.value,4)
+  
+  entry=entry+1
+  
+  #if(ad$p.value<0.05 | cvm$p.value<0.05){
+  #  print(paste("Log-Logistic does not fit at SQ:",i))
+  # }
+  #  cat("\n\n")
+  
+}
+
+
+
 ## eONKI4
 
 coef_var=rep(0,length(levels(Dat$SQf)))
@@ -321,7 +323,7 @@ for(i in levels(Dat$SQf)){
   sub.quant= Dat.eONKI %>%
     filter(SQf==i)
   
- # qqnorm(sub.quant$Cq,main=paste("SQ",i))
+  # qqnorm(sub.quant$Cq,main=paste("SQ",i))
   #qqline(sub.quant$Cq)
   
   sub.quant=sub.quant[!(is.na(sub.quant$Cq)),]
@@ -341,10 +343,10 @@ for(i in levels(Dat$SQf)){
   entry=entry+1
   
   #if(ad$p.value<0.05 | cvm$p.value<0.05){
- #   print("stop")
- # }
+  #   print("stop")
+  # }
   
- # cat("\n\n")
+  # cat("\n\n")
 }
 #dev.off()
 
@@ -380,7 +382,7 @@ for(i in levels(Dat$SQf)){
   dat.weib=fitdist(sub.quant$Cq,"weibull")
   
   #qqPlot(qweibull(ppoints(length(sub.quant$Cq)), shape = dat.weib$estimate[1], 
-   #               scale =  dat.weib$estimate[2]), sub.quant$Cq,add.line=TRUE,xlab="Theoretical Quantiles",ylab="Sample Quantiles",main=paste("SQ",i))
+  #               scale =  dat.weib$estimate[2]), sub.quant$Cq,add.line=TRUE,xlab="Theoretical Quantiles",ylab="Sample Quantiles",main=paste("SQ",i))
   
   
   mle.shape=dat.weib$estimate[1] #alpha
@@ -413,10 +415,10 @@ for(i in levels(Dat$SQf)){
   
   
   
- # if(ad$p.value<0.05 | cvm$p.value<0.05){
+  # if(ad$p.value<0.05 | cvm$p.value<0.05){
   #  print(paste("Weibull does not fit at SQ:",i))
- # }
- # cat("\n\n")
+  # }
+  # cat("\n\n")
   
 }
 #dev.off()
@@ -444,7 +446,7 @@ for(i in levels(Dat$SQf)){
   
   
   #qqPlot(qllogis(ppoints(length(sub.quant$Cq)), shape = dat.loglogis$estimate[1], 
-    #             scale =  dat.loglogis$estimate[2]), log(sub.quant$Cq),add.line=TRUE,xlab="Theoretical Quantiles",ylab="Sample Quantiles",main=paste("SQ",i))
+  #             scale =  dat.loglogis$estimate[2]), log(sub.quant$Cq),add.line=TRUE,xlab="Theoretical Quantiles",ylab="Sample Quantiles",main=paste("SQ",i))
   
   
   mle.shape=as.numeric(dat.loglogis$estimate[1]) #Beta
@@ -470,7 +472,7 @@ for(i in levels(Dat$SQf)){
   
   coef_var[entry]=round(sd(sub.quant$Cq)/mean(sub.quant$Cq),4)
   
-
+  
   
   cvm=cvm.test(sub.quant$Cq, "pllogis", shape = dat.loglogis$estimate[1], scale = dat.loglogis$estimate[2])
   ad=ad.test(sub.quant$Cq, "pllogis", shape = dat.loglogis$estimate[1], scale = dat.loglogis$estimate[2])
@@ -484,9 +486,9 @@ for(i in levels(Dat$SQf)){
   
   
   #if(ad$p.value<0.05 | cvm$p.value<0.05){
- #   print(paste("Log-Logistic does not fit at SQ:",i))
- # }
- # cat("\n\n")
+  #   print(paste("Log-Logistic does not fit at SQ:",i))
+  # }
+  # cat("\n\n")
   
 }
 
@@ -523,17 +525,217 @@ for(t in full_targets){
   entry=1 # this is used to loop over and populate the vectors
   
   
-
+  
   # SQ of  1, 2 and 3 were only for eONKI4 and eFISH1 so we can't loop over these.
   # Dat
   
   for(i in levels(Dat.new$SQf)){ # Loop over all SQ of intrest
     
     sub.quant=Dat.e %>% filter(SQf==i)
-
+    
   }
   cat("\n\n")
 }
+# This script produces the various qqplot under several distributions for multiple eDNA targets. We focus on eFISH1 and eONKI4 but include
+#normal qqplots for other targets as well.
+
+
+# eFISH1
+
+par(mfrow=c(3,2))
+
+for(i in levels(Dat$SQf)){ # Loop over all SQ of intrest
+  
+  sub.quant= Dat.eFISH %>%
+    filter(SQf==i)
+  
+  qqnorm(sub.quant$Cq,main=paste("Normal QQ-Plot:","SQ",i),sub='Target: eFISH1') # Make the Q-Q plot
+  qqline(sub.quant$Cq) #Add the Q-Q line
+}
+
+
+par(mfrow=c(3,2))
+
+for(i in levels(Dat$SQf)){
+  
+  sub.quant= Dat.eFISH %>%
+    filter(SQf==i)
+  
+  sub.quant=sub.quant[!(is.na(sub.quant$Cq)),] #Remove missing entries
+  
+  qqnorm(log(sub.quant$Cq),main=paste("Log-Normal QQ-Plot:","SQ",i),sub='Target: eFISH1')
+  qqline(log(sub.quant$Cq))
+  
+}
+
+
+
+par(mfrow=c(3,2))
+
+for(i in levels(Dat$SQf)){
+  
+  sub.quant= Dat.eFISH %>%
+    filter(SQf==i) %>%
+    filter(!is.na(Cq))
+  
+  sub.quant=sub.quant[!(is.na(sub.quant$Cq)),]
+  
+  dat.weib=fitdist(sub.quant$Cq,"weibull") #Fit a Weibull distribution using fitdist to estimate the parameters
+  
+  qqPlot(qweibull(ppoints(length(sub.quant$Cq)), shape = dat.weib$estimate[1], 
+                  scale =  dat.weib$estimate[2]), sub.quant$Cq,add.line=TRUE,xlab="Theoretical Quantiles",ylab="Sample Quantiles",main=paste("Weibull QQ-Plots:","SQ",i),sub='Target: eFISH1')
+  
+}
+
+
+
+
+par(mfrow=c(3,2))
+
+for(i in levels(Dat$SQf)){
+  
+  sub.quant= Dat.eFISH %>%
+    filter(SQf==i) %>%
+    filter(!is.na(Cq))
+  
+  sub.quant=sub.quant[!(is.na(sub.quant$Cq)),]
+  
+  
+  dat.loglogis=fitdist(sub.quant$Cq,"llogis")
+  
+  qqPlot(qllogis(ppoints(length(sub.quant$Cq)), shape = dat.loglogis$estimate[1], 
+                 scale =  dat.loglogis$estimate[2]), log(sub.quant$Cq),add.line=TRUE,xlab="Theoretical Quantiles",ylab="Sample Quantiles",main=paste("Log-Logistic QQ-Plots:","SQ",i),sub='Target: eFISH1')
+  
+}
+
+
+
+# eONKI4
+
+Dat.eONKI= Dat %>%
+  filter(Target=='eONKI4')
+
+par(mfrow=c(3,2))
+
+for(i in levels(Dat$SQf)){
+  
+  sub.quant= Dat.eONKI %>%
+    filter(SQf==i)
+  
+  qqnorm(sub.quant$Cq,main=paste("Normal QQ-Plot:","SQ",i),sub="Target: eONKI4")
+  qqline(sub.quant$Cq)
+  
+}
+
+
+par(mfrow=c(3,2))
+
+for(i in levels(Dat$SQf)){
+  
+  sub.quant= Dat.eONKI %>%
+    filter(SQf==i)
+  
+  qqnorm(log(sub.quant$Cq),main=paste("Log-Normal QQ-Plot:","SQ",i),sub="Target: eONKI4")
+  qqline(log(sub.quant$Cq))
+  
+}
+
+
+
+par(mfrow=c(3,2))
+
+
+for(i in levels(Dat$SQf)){
+  
+  sub.quant= Dat.eONKI %>%
+    filter(SQf==i) %>%
+    filter(!is.na(Cq)) #We don't include missing NA values.
+  
+  sub.quant=sub.quant[!(is.na(sub.quant$Cq)),]
+  
+  
+  quant.mean=mean(sub.quant$Cq)
+  quant.sd=sd(sub.quant$Cq)
+  
+  
+  dat.weib=fitdist(sub.quant$Cq,"weibull")
+  
+  qqPlot(qweibull(ppoints(length(sub.quant$Cq)), shape = dat.weib$estimate[1], 
+                  scale =  dat.weib$estimate[2]), sub.quant$Cq,add.line=TRUE,xlab="Theoretical Quantiles",ylab="Sample Quantiles",main=paste("Weibull QQ-Plot:","SQ",i),sub="Target eONKI4")
+}
+
+
+par(mfrow=c(3,2))
+
+for(i in levels(Dat$SQf)){
+  
+  sub.quant= Dat.eONKI %>%
+    filter(SQf==i)
+  
+  sub.quant=sub.quant[!(is.na(sub.quant$Cq)),]
+  
+  
+  dat.loglogis=fitdist(sub.quant$Cq,"llogis",lower=c(0,0)) #Fit a Log-logistic distribution using fitdist
+  
+  
+  
+  qqPlot(qllogis(ppoints(length(sub.quant$Cq)), shape = dat.loglogis$estimate[1], 
+                 scale =  dat.loglogis$estimate[2]), log(sub.quant$Cq),add.line=TRUE,xlab="Theoretical Quantiles",ylab="Sample Quantiles",main=paste('Log-Logistic QQ-Plot',"SQ",i),sub="Target: eONKI4")
+  
+  
+}
+
+
+# Other Targets
+
+full_targets=c("eASMO9","eASTR4","eLIPI1", "eMIDO1","eMISA2","eRAAU1", "eRACA2","eRALU2", "eRAPR2")
+
+
+Dat.new=Dat%>%
+  filter(Target!='eONKI4')%>%
+  filter(Target!='eFISH1')%>%
+  filter(SQf!=1)%>%
+  filter(SQf!=2)%>%
+  filter(SQf!=3)
+
+
+invisible(droplevels(Dat.new$SQf))
+Dat.new$SQf=factor(Dat.new$SQf)
+
+
+for(t in full_targets){
+  
+  Dat.e= Dat.new %>%
+    filter(Target==t) #Restrict attention to each target
+  
+  
+  coef_var=rep(0,length(levels(Dat$SQf))) #Initalize a vector to store the regular cvs
+  
+  coef_var_unbiased=rep(0,length(levels(Dat$SQf))) #Initalize a vector to store the unbiased cvs
+  
+  p_val.cvm=rep(0,length(levels(Dat$SQf))) #Initalize a vector to store the p values from the cvm test
+  p_val.ad=rep(0,length(levels(Dat$SQf))) #Initalize a vector to store the p values from the ad test
+  
+  entry=1 # this is used to loop over and populate the vectors
+  
+  
+  par(mfrow=c(3,3)) #Set plotting parameters
+  
+  # SQ of  1, 2 and 3 were only for eONKI4 and eFISH1 so we can't loop over these.
+  # Dat
+  
+  for(i in levels(Dat.new$SQf)){ # Loop over all SQ of intrest
+    
+    sub.quant=Dat.e %>% filter(SQf==i)
+    #sub.quant=Dat.e%>%filter(!(is.na(Cq)))
+    
+    qqnorm(sub.quant$Cq,main=paste("SQ",i,"","Target",t),sub="Normal QQ Plot") # Make the Q-Q plot
+    qqline(sub.quant$Cq) #Add the Q-Q line
+  }
+}
+
+
+
 
 
 
